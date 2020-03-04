b1d22a9c85327d020b2d5d30b9b3683f40e61ecc
Nino Ross Rodriguez <ninoross.rodriguez@adelphi.digital>
2020-03-05 10:20:04 +1100

    - added back AR models

--- src/_js/main.js
+++ src/_js/main.js
@@ -16,28 +16,33 @@ import $ from 'jquery';
 // import MaterialDesign from './common/_material-design';
 import ServiceWorker from '../_modules/serviceworker/serviceworker';
 
+let isAframePresent = false;
 let isReady = false;
 let isPlaying = false;
 let triggerPlay = false;
 let videoBeacons = '';
 
-AFRAME.registerComponent('markerhandler', {
-    init: function () {
-        // Set up the tick throttling. Will check if marker is active every 500ms
-        this.tick = AFRAME.utils.throttleTick(this.tick, 200, this);
-    },
-    tick: function (t, dt) {
-        if (isReady) {
-            if (document.querySelector("#hiroMarker").object3D.visible && !isPlaying) {
-                console.log('Marker is present');
-                playVideo();
-            } else if (!document.querySelector("#hiroMarker").object3D.visible && isPlaying) {
-                console.log('Marker is not present');
-                stopVideo();
+if (typeof AFRAME !== 'undefined') {
+    isAframePresent = true;
+    AFRAME.registerComponent('markerhandler', {
+        init: function () {
+            // Set up the tick throttling. Will check if marker is active every 500ms
+            this.tick = AFRAME.utils.throttleTick(this.tick, 200, this);
+        },
+        tick: function (t, dt) {
+            if (isReady) {
+                if (document.querySelector("#hiroMarker").object3D.visible && !isPlaying) {
+                    console.log('Marker is present');
+                    playVideo();
+                } else if (!document.querySelector("#hiroMarker").object3D.visible && isPlaying) {
+                    console.log('Marker is not present');
+                    stopVideo();
+                }
             }
         }
-    }
-});
+    });
+}
+
 
 $(() => {
     // Init Material Design
@@ -48,17 +53,19 @@ $(() => {
     // Simple Service Worker to make App Install work (OPTIONAL)
     // new ServiceWorker();
 
-    videoBeacons = document.getElementById('beacons');
+    if (isAframePresent) {
+        videoBeacons = document.getElementById('beacons');
 
-    document.addEventListener('contextmenu', function (e) {
-        e.preventDefault()
-    });
+        document.addEventListener('contextmenu', function (e) {
+            e.preventDefault()
+        });
 
-    window.addEventListener('touchend', function () {
-        triggerPlay = true;
-    }, false);
+        window.addEventListener('touchend', function () {
+            triggerPlay = true;
+        }, false);
 
-    isReady = true;
+        isReady = true;
+    }
 
 
     console.log("I'm a AR!!!");
