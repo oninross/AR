386e02ba41b623ab9d7ec421e11448d4fde3e62c
Nino Ross Rodriguez <oninross@gmail.com>
2021-01-08 21:29:14 +1100

    Updated package file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,7 +6,6 @@ import browserSyncLib from 'browser-sync';
 import pjson from './package.json';
 import minimist from 'minimist';
 import glob from 'glob';
-import shell from 'gulp-shell';
 
 // Load all gulp plugins based on their names
 // EX: gulp-copy -> copy
@@ -37,11 +36,6 @@ glob.sync('./gulp/**/*.js').filter(function(file) {
 }).map(function(file) {
   require(file)(gulp, plugins, args, config, taskTarget, browserSync);
 });
-
-// Firebase deploy
-gulp.task('firebase', shell.task([
-  'firebase deploy'
-]))
 
 // Default task
 gulp.task('default', ['clean'], () => {
